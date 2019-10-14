577c7a59e294a453f5f70339474976045d092910
eeeeaa <tigerinwmak@gmail.com>
2019-10-14 15:58:42 +0700

    First Commit

---
+++ ProjectClient/GridGame/GridPoint.gd
@@ -0,0 +1,33 @@
+extends Area2D
+
+onready var tex1 = load("res://Asset/d1.png")
+onready var tex2 = load("res://Asset/d2.png")
+onready var tex3 = load("res://Asset/d4.png")
+onready var game = get_tree().get_root().get_node("GridGame")
+onready var tex_select = load("res://Asset/d_select.png")
+var index = 0
+func _ready():
+	$Sprite.set_texture(tex3)
+	$Select.modulate.a = 0.2
+func clear_tex():
+	$Sprite.set_texture(tex3)
+	index = 0
+func get_tex(index):
+	match index:
+		0:return tex1
+		1:return tex2
+		2:return tex3
+func _on_GridPoint_mouse_entered():
+	$Select.set_texture(tex_select)
+func _on_GridPoint_mouse_exited():
+	$Select.set_texture(null)
+func get_current_tex():
+	return index
+func _on_GridPoint_input_event(viewport, event, shape_idx):
+	if Input.is_action_just_pressed("mouse"):
+		print(str(index))
+		$Sprite.set_texture(get_tex(index))
+		index = (index+1)%3
+		rpc("update_tex",get_tex(index))
+sync func update_tex(new_index):
+	$Sprite.set_texture(new_index)
---
+++ ProjectServer/GridGame/network.gd
@@ -0,0 +1,67 @@
+extends Node
+
+# Default game port
+const DEFAULT_PORT = 5000
+
+# Max number of players
+const MAX_PLAYERS = 2
+
+# Players dict stored as id:name
+var player_dict = {}
+var ready_players = []
+
+func _ready():
+	get_tree().connect("network_peer_connected", self, "_player_connected")
+	get_tree().connect("network_peer_disconnected", self,"_player_disconnected")
+	
+	create_server()
+
+func create_server():
+	var host = NetworkedMultiplayerENet.new()
+	host.create_server(DEFAULT_PORT, MAX_PLAYERS)
+	get_tree().set_network_peer(host)
+func _player_connected(id):
+	print(str(id) + " connected")
+func _player_disconnected(id):
+	print(str(id) + " disconnected")
+remote func register_player(new_player_name):
+	# We get id this way instead of as parameter, to prevent users from pretending to be other users
+	var caller_id = get_tree().get_rpc_sender_id()
+	
+	# If game is going, just ignore new guy
+	if not has_node("/root/GridGame"):
+		# Add him to our list
+		player_dict[caller_id] = new_player_name
+		
+		# Add everyone to new player:
+		for p_id in player_dict:
+			rpc_id(caller_id, "register_player", p_id, player_dict[p_id]) # Send each player to new dude
+		
+		rpc("register_player", caller_id, player_dict[caller_id]) # Send new dude to all players
+		# NOTE: this means new player's register gets called twice, but fine as same info sent both times
+		
+		print("Client ", caller_id, " registered as ", new_player_name)
+puppetsync func unregister_player(id):
+	player_dict.erase(id)
+	
+	print("Client ", id, " was unregistered")
+remote func player_ready():
+	var caller_id = get_tree().get_rpc_sender_id()
+	
+	if not ready_players.has(caller_id):
+		ready_players.append(caller_id)
+		
+	if ready_players.size() == player_dict.size():
+		pre_start_game()
+func pre_start_game():
+	var world = load("res://GridGame/GridGame.tscn").instance()
+	get_tree().get_root().add_child(world)
+	for id in player_dict:
+		get_node("/root/GridGame").spawn_player(id)
+	rpc("pre_start_game")
+remote func post_start_game():
+	var caller_id = get_tree().get_rpc_sender_id()
+	var world = get_node("/root/GridGame")
+	for player in world.get_node("Players").get_children():
+		world.rpc_id(caller_id, "spawn_player", player.get_network_master())
+	
